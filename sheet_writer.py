71ec875ef71d5c195a0e11dda1316d8dbd49695e
sanjeevk488-design <sanjeevk488@gmail.com>
2026-06-03 13:17:25 +0530

    Update sheet_writer.py

--- sheet_writer.py
+++ sheet_writer.py
@@ -8,12 +8,18 @@ SCOPES = [
 ]
 
 def get_sheet():
+
     creds = Credentials.from_service_account_file(
         "credentials.json",
         scopes=SCOPES
     )
 
     client = gspread.authorize(creds)
+
+    print("Trying Sheet ID:", SHEET_ID)
+
     spreadsheet = client.open_by_key(SHEET_ID)
+
+    print("Opened Sheet:", spreadsheet.title)
 
     return spreadsheet
